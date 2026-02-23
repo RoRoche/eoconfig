c71cc817e7aa6b0854e14147d6c574344931c26c
Romain Rochegude <romain.rochegude@gmail.com>
2026-02-23 19:25:32 +0100

    :wrench: configure commitlint for gitmoji. See #2

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  rules: {
+
+    // Type could be empty
+    'type-empty': [0],
+
+    // Subject could be empty
+    'subject-empty': [0],
+
+    // All types are accepted
+    'type-enum': [0],
+
+    // No need to limit the length of the header
+    'header-max-length': [2, 'always', 120]
+  }
+};
