9230c06b79d86d805fa7b7096a286ee50de12749
Romain Rochegude <romain.rochegude@gmail.com>
2026-05-04 19:31:05 +0200

    :label: new types to encapsulate logic. See #170

---
+++ src/main/java/com/github/roroche/eoconfig/ParsedYaml.java
@@ -0,0 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Romain Rochegude
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.roroche.eoconfig;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.cactoos.Input;
+import org.cactoos.Scalar;
+import org.cactoos.io.InputStreamOf;
+
+/**
+ * A representation of parsed YAML content as a map.
+ * @since 0.0.5
+ */
+public final class ParsedYaml implements Scalar<Map<String, Object>> {
+
+    /**
+     * The Jackson mapper to use.
+     */
+    private final ObjectMapper mapper;
+
+    /**
+     * The input to parse.
+     */
+    private final Input input;
+
+    /**
+     * Primary ctor.
+     * @param mapper The Jackson mapper to use
+     * @param input The input to parse
+     */
+    public ParsedYaml(final ObjectMapper mapper, final Input input) {
+        this.mapper = mapper;
+        this.input = input;
+    }
+
+    @SuppressWarnings({"allfinal", "PMD.UnnecessaryLocalRule", "PMD.LooseCoupling"})
+    @Override
+    public java.util.Map<String, Object> value() throws Exception {
+        try (InputStream stream = new InputStreamOf(this.input)) {
+            return this.mapper.readValue(
+                stream,
+                new TypeReference<LinkedHashMap<String, Object>>() { }
+            );
+        }
+    }
+}
---
+++ src/main/java/com/github/roroche/eoconfig/YamlMap.java
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Romain Rochegude
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.roroche.eoconfig;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.cactoos.Input;
+import org.cactoos.Scalar;
+import org.cactoos.map.MapEnvelope;
+import org.cactoos.scalar.Sticky;
+
+/**
+ * A {@link Map} built from YAML.
+ * @since 0.0.5
+ */
+public final class YamlMap extends MapEnvelope<String, Object> {
+
+    /**
+     * Secondary ctor.
+     * @param mapper The Jackson mapper to use
+     * @param input The input to parse
+     */
+    public YamlMap(final ObjectMapper mapper, final Input input) throws Exception {
+        this(
+            new Sticky<>(
+                new ParsedYaml(mapper, input)
+            )
+        );
+    }
+
+    /**
+     * Primary ctor.
+     * @param origin The original {@link Map} from YAML
+     */
+    /*
+     * @checkstyle ConstructorsCodeFreeCheck (4 lines)
+     */
+    public YamlMap(final Scalar<Map<String, Object>> origin) throws Exception {
+        super(origin.value());
+    }
+}
